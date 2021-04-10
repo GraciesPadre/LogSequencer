b336c908e8f0ce79c82ea8d3bb1dd04a7a0d83f9
GraciesPadre <ShastasPadre1@gmail.com>
2021-04-10 15:30:05 -0600

    Initial commit

---
+++ app/src/main/java/org/doomer/logsequencer/MainActivity.kt
@@ -0,0 +1,18 @@
+package org.doomer.logsequencer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.doomer.logsequencer.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/org/doomer/logsequencer/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package org.doomer.logsequencer.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
